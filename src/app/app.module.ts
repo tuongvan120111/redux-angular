f205215f1d39b02cc99e7231bdb7043123b8edd1
Van Tang <tuongvan120111@gmail.com>
2022-08-25 17:12:06 +0700

    Run code getting started of ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppComponent } from './app.component';
 import { ToDoComponent } from './components/to-do/to-do.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
      ToDoComponent,
-    DashboardComponent
+    DashboardComponent,
+    MyCounterComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
